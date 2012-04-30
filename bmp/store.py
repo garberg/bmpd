0d132253b355cab6f6111a8206b55ad8f823b6c2
Lukas Garberg <lukas@spritelink.net>
2012-04-30 19:19:26 +0200

    Moved SQL queries and revamped some logging msgs
    SQL queries are now defined globally.

--- bmp/store.py
+++ bmp/store.py
@@ -9,6 +9,49 @@ import time
 
 import BMP
 
+Q_INSERT_RAW = """INSERT INTO raw_log
+    (ts, bmp_src, bgp_src, bgp_data)
+    VALUES
+    (%s, %s, %s, %s)"""
+
+Q_FIND_PATH = """SELECT id FROM adj_rib_in
+    WHERE
+        bmp_src = %s
+        AND neighbor_addr = %s
+        AND prefix = %s
+        AND contains(valid, CURRENT_TIMESTAMP)"""
+
+Q_INVALIDATE_PATH = """UPDATE adj_rib_in SET
+    valid = ( '[' || first(valid) || ', ' || %s || ' )' )::period
+    WHERE id = %s"""
+
+Q_INSERT_PATH = """INSERT INTO adj_rib_in
+    (
+        valid,
+        bmp_src,
+        neighbor_addr,
+        neighbor_as,
+        next_hop,
+        lpref,
+        origin,
+        med,
+        prefix,
+        aspath,
+        communities
+    ) VALUES (
+        ('[ ' || %(time)s || ', ' || (%(time)s + interval '1000 years') || ' )')::period,
+        %(bmp_src)s,
+        %(neighbor_addr)s,
+        %(neighbor_as)s,
+        %(next_hop)s,
+        %(lpref)s,
+        %(origin)s,
+        %(med)s,
+        %(prefix)s,
+        %(aspath)s,
+        %(communities)s
+    )"""
+
 class Store:
 
 
@@ -18,8 +61,10 @@ class Store:
     time_update = 0.0
     time_insert = 0.0
     nmsg = 0
-    npref = 0
-    last_npref = 0
+    npref_add = 0
+    last_npref_add = 0
+    npref_del = 0
+    last_npref_del = 0
     nmsg = 0
     last_ts = None
 
@@ -36,9 +81,6 @@ class Store:
         # save BMP message
 
         if msg.msg_type == BMP.MSG_TYPE_ROUTE_MONITORING:
-            #self._logger.debug("Got a route monitoring message")
-
-            #self._logger.debug("nlri: %s withdraw: %s patattr: %s" % (msg.update.nlri, msg.update.withdraw, msg.update.pathattr))
             self.store_route_mon(msg, src)
 
 
@@ -49,12 +91,10 @@ class Store:
             raise Exception('lollerskates')
 
         elif msg.msg_type == BMP.MSG_TYPE_PEER_DOWN_NOTIFICATION:
-            pass
-            #self._logger.debug("Got a peer down notification message")
+            self._logger.debug("Got a peer down notification message")
 
         else:
-            pass
-            #self._logger.debug("Other message")
+            self._logger.debug("Other message")
 
 
 
@@ -67,63 +107,28 @@ class Store:
 
 
         # save raw BGP packet
-        sql = """INSERT INTO raw_log (ts, bmp_src, bgp_src, bgp_data) VALUES (%s, %s, %s, %s)"""
-        self.curs.execute(sql, (msg.time, src.host, msg.peer_address, psycopg2.Binary(msg.raw_payload)))
+        self.curs.execute(Q_INSERT_RAW, (msg.time, src.host, msg.peer_address, psycopg2.Binary(msg.raw_payload)))
 
         #
         # update adj_rib_in table
 
-        # nlri
-        find_path = """SELECT id FROM adj_rib_in
-            WHERE
-                bmp_src = %s
-                AND neighbor_addr = %s
-                AND prefix = %s
-                AND contains(valid, CURRENT_TIMESTAMP)"""
-        invalidate_prev = """UPDATE adj_rib_in SET valid = ( '[' || first(valid) || ', ' || %s || ' )' )::period WHERE id = %s"""
-        insert_path = """INSERT INTO adj_rib_in
-            (
-                valid,
-                bmp_src,
-                neighbor_addr,
-                neighbor_as,
-                next_hop,
-                lpref,
-                origin,
-                med,
-                prefix,
-                aspath,
-                communities
-            ) VALUES (
-                ('[ ' || %(time)s || ', ' || (%(time)s + interval '1000 years') || ' )')::period,
-                %(bmp_src)s,
-                %(neighbor_addr)s,
-                %(neighbor_as)s,
-                %(next_hop)s,
-                %(lpref)s,
-                %(origin)s,
-                %(med)s,
-                %(prefix)s,
-                %(aspath)s,
-                %(communities)s
-            )"""
-
         # withdrawals
         for pref in msg.update.withdraw:
-            pass
+            self._logger.debug("Withdrawal of prefix %s" % pref)
+            self.npref_del += 1
 
         # nlri
         for pref in msg.update.nlri:
 
             # does prefix exist? If so, end validity.
             start_select = time.time()
-            self.curs.execute(find_path, (src.host, msg.peer_address, pref.prefix))
+            self.curs.execute(Q_FIND_PATH, (src.host, msg.peer_address, pref.prefix))
             self.time_select += time.time() - start_select
             if self.curs.rowcount > 0:
                 row = self.curs.fetchone()
                 start_update = time.time()
                 try:
-                    self.curs.execute(invalidate_prev, (msg.time, row[0]))
+                    self.curs.execute(Q_INVALIDATE_PATH, (msg.time, row[0]))
                 except psycopg2.Error, e:
                     self._logger.error("Unable to invalidate prefix %s (id %d) in message %s: %s " % (pref, row[0], msg, e))
                     self._logger.error("Time: %s" % msg.time)
@@ -164,26 +169,29 @@ class Store:
                 args['communities'] = msg.update.pathattr.get('extcommunity').value
 
             start_insert = time.time()
-            self.curs.execute(insert_path, args)
+            self.curs.execute(Q_INSERT_PATH, args)
             self.time_insert += time.time() - start_insert
 
-            self.npref += 1
+            self.npref_add += 1
 
         self.nmsg += 1
 
         if self.nmsg % 10000 == 0:
             now = time.time()
-            self._logger.debug('nmsg %d %.1f msg/s; npref %d %.1f pref/s select: %f update: %f insert: %f' % (
+            self._logger.debug('nmsg %d %.1f msg/s; npref_add %d %.1f pref/s npref_del %d %.1f pref/s select: %f update: %f insert: %f' % (
                 self.nmsg,
                 10000/(now - self.last_ts),
-                self.npref,
-                (self.npref - self.last_npref)/(now - self.last_ts),
+                self.npref_add,
+                (self.npref_add - self.last_npref_add)/(now - self.last_ts),
+                self.npref_del,
+                (self.npref_del - self.last_npref_del)/(now - self.last_ts),
                 self.time_select,
                 self.time_update,
                 self.time_insert
             ))
             self.last_ts = now
-            self.last_npref = self.npref
+            self.last_npref_add = self.npref_add
+            self.last_npref_del = self.npref_del
 
         self.conn.commit()
 
